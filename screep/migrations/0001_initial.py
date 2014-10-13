f8e6ff3769c42367a5265bf43fac431079eec70c
Adriaan Tijsseling <adriaan@tijsseling.com>
2014-10-13 17:16:50 +0200

    New migration

--- screep/migrations/0001_initial.py
+++ screep/migrations/0001_initial.py
@@ -1,86 +1,66 @@
 # -*- coding: utf-8 -*-
-from south.utils import datetime_utils as datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+import screep.helpers
+import django.utils.timezone
+import model_utils.fields
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'CrawlConfig'
-        db.create_table(u'screep_crawlconfig', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('key', self.gf('django.db.models.fields.CharField')(unique=True, max_length=50, db_index=True)),
-            ('value', self.gf('django.db.models.fields.CharField')(default='', max_length=200)),
-        ))
-        db.send_create_signal(u'screep', ['CrawlConfig'])
+    dependencies = [
+    ]
 
-        # Adding model 'CrawlDomain'
-        db.create_table(u'screep_crawldomain', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('created', self.gf('model_utils.fields.AutoCreatedField')(default=datetime.datetime.now)),
-            ('modified', self.gf('model_utils.fields.AutoLastModifiedField')(default=datetime.datetime.now)),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('domain', self.gf('screep.helpers.DomainNameField')(db_index=True, unique=True, max_length=128, blank=True)),
-            ('status', self.gf('django.db.models.fields.IntegerField')(default=0, db_index=True)),
-            ('ttl', self.gf('django.db.models.fields.IntegerField')(default=24)),
-            ('lastcrawl', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime(1970, 1, 1, 0, 0))),
-        ))
-        db.send_create_signal(u'screep', ['CrawlDomain'])
-
-        # Adding index on 'CrawlDomain', fields ['ttl', 'lastcrawl']
-        db.create_index(u'screep_crawldomain', ['ttl', 'lastcrawl'])
-
-        # Adding model 'DomainAttributes'
-        db.create_table(u'screep_domainattributes', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('key', self.gf('django.db.models.fields.CharField')(unique=True, max_length=50)),
-            ('xpath', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('domain', self.gf('django.db.models.fields.related.ForeignKey')(related_name='domain_attributes', to=orm['screep.CrawlDomain'])),
-        ))
-        db.send_create_signal(u'screep', ['DomainAttributes'])
-
-
-    def backwards(self, orm):
-        # Removing index on 'CrawlDomain', fields ['ttl', 'lastcrawl']
-        db.delete_index(u'screep_crawldomain', ['ttl', 'lastcrawl'])
-
-        # Deleting model 'CrawlConfig'
-        db.delete_table(u'screep_crawlconfig')
-
-        # Deleting model 'CrawlDomain'
-        db.delete_table(u'screep_crawldomain')
-
-        # Deleting model 'DomainAttributes'
-        db.delete_table(u'screep_domainattributes')
-
-
-    models = {
-        u'screep.crawlconfig': {
-            'Meta': {'object_name': 'CrawlConfig'},
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'key': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '50', 'db_index': 'True'}),
-            'value': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '200'})
-        },
-        u'screep.crawldomain': {
-            'Meta': {'object_name': 'CrawlDomain', 'index_together': "[['ttl', 'lastcrawl']]"},
-            'created': ('model_utils.fields.AutoCreatedField', [], {'default': 'datetime.datetime.now'}),
-            'domain': ('screep.helpers.DomainNameField', [], {'db_index': 'True', 'unique': 'True', 'max_length': '128', 'blank': 'True'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'lastcrawl': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(1970, 1, 1, 0, 0)'}),
-            'modified': ('model_utils.fields.AutoLastModifiedField', [], {'default': 'datetime.datetime.now'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'status': ('django.db.models.fields.IntegerField', [], {'default': '0', 'db_index': 'True'}),
-            'ttl': ('django.db.models.fields.IntegerField', [], {'default': '24'})
-        },
-        u'screep.domainattributes': {
-            'Meta': {'object_name': 'DomainAttributes'},
-            'domain': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'domain_attributes'", 'to': u"orm['screep.CrawlDomain']"}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'key': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '50'}),
-            'xpath': ('django.db.models.fields.CharField', [], {'max_length': '255'})
-        }
-    }
-
-    complete_apps = ['screep']
+    operations = [
+        migrations.CreateModel(
+            name='CrawlConfig',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('key', models.CharField(unique=True, max_length=50, db_index=True)),
+                ('value', models.CharField(default=b'', max_length=200)),
+            ],
+            options={
+                'verbose_name': 'crawl configuration item',
+                'verbose_name_plural': 'crawl configuration items',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='CrawlDomain',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created', model_utils.fields.AutoCreatedField(default=django.utils.timezone.now, verbose_name='created', editable=False)),
+                ('modified', model_utils.fields.AutoLastModifiedField(default=django.utils.timezone.now, verbose_name='modified', editable=False)),
+                ('name', models.CharField(max_length=255)),
+                ('domain', screep.helpers.DomainNameField(db_index=True, unique=True, max_length=128, blank=True)),
+                ('status', models.IntegerField(default=0, db_index=True, choices=[(0, b'ok'), (1, b'error'), (2, b'disabled')])),
+                ('ttl', models.IntegerField(default=24, help_text=b'Interval between crawls in hours.')),
+                ('lastcrawl', models.DateTimeField(default=datetime.datetime(1970, 1, 1, 0, 0))),
+            ],
+            options={
+                'verbose_name': 'crawl domain',
+                'verbose_name_plural': 'crawl domains',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='DomainAttributes',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('key', models.CharField(unique=True, max_length=50)),
+                ('xpath', models.CharField(max_length=255)),
+                ('domain', models.ForeignKey(related_name=b'domain_attributes', to='screep.CrawlDomain')),
+            ],
+            options={
+                'verbose_name': 'domain attribute',
+                'verbose_name_plural': 'domain attributes',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AlterIndexTogether(
+            name='crawldomain',
+            index_together=set([('ttl', 'lastcrawl')]),
+        ),
+    ]
